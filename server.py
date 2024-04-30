c04013272546a2d7811e69aa66bb939c97824e26
giladtwili4real <giladtwili@hotmail.com>
2024-04-30 15:04:36 +0300

    fetch_bestbuy

--- server.py
+++ server.py
@@ -1,54 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 
-# Global list to store all items
-bestbuy_items = []
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
+]
+headers = {'User-Agent': random.choice(user_agents)}
 
-proxy = {
-    'http': "username:password@host",
-    'https': "username:password@host"
-}
+items = []
 
 def fetch_bestbuy(query):
-    current_page = 1
-    proceed = True
-    
-    while proceed and current_page < 3:
-        # Modified URL to include page number
-        url = f"https://www.bestbuy.com/site/searchpage.jsp?cp={current_page}&st={query}"
-        page = requests.get(url, proxies=proxy)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        
+    try:
+        # Perform the search
+        url = f"https://www.bestbuy.com/site/searchpage.jsp?intl=nosplash&st={query}"
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
         # Check if the page is valid and contains products
-        if "undefined - Best Buy" in soup.title.text or not soup.find_all('li', class_="sku-item"):
-            proceed = False
+        if "undefined - Best Buy" in soup.title.text:
+            return False 
         else:
-            cp_products = soup.find_all("li", class_="sku-item")
-            for product in cp_products:
-                # Corrected to find elements within each product
-                title_element = product.find("img").attrs["alt"]
-                price_element = product.find("class",class_ = "sr-only")
+            product = soup.find("li", class_="sku-item")
+            link_element = product.find("a", class_="image-link")
+            title_element = product.find("h4", class_="sku-title")
+            price_element = product.find("div", class_=["priceView-hero-price","priceView-customer-price"]).findChildren()[0]
+            if link_element and title_element and price_element:
+                link = link_element['href'] 
+                title = title_element.get_text(strip=True)
+                price = price_element.get_text(strip=True)
+                item = {
+                    'site': 'BestBuy',
+                    'link': f"https://www.bestbuy.com{link}&intl=nosplash",
+                    'item_title_name': title,
+                    'price': price
+                }
+        return item
+    except:
+        return False
+                
+   
 
-                print(title_element)
-                print(price_element)
-                if title_element and price_element:
-                    title = title_element.get_text(strip=True)
-                    price = price_element.get_text(strip=True)
-                    item = {
-                        'site': 'BestBuy',
-                        'item_title_name': title,
-                        'price': price
-                    }
-                    bestbuy_items.append(item)
-            current_page += 1
-
-    return bestbuy_items
-
-# Example usage
 product_name = input("Enter the product name: ")
-fetch_bestbuy(product_name)
-for item in bestbuy_items:
-    print(item)
+item = fetch_bestbuy(product_name)
+print(item)
 
 
 # def fetch_walmart(query):
